2338ce648f0a8f112b89c4b4285505cc080f300f
japric15-hub <japric15@asu.edu>
2025-10-21 15:11:09 -0700

    image check boxes

--- src/gui/installer/images.cpp
+++ src/gui/installer/images.cpp
@@ -3,8 +3,21 @@
 #include "imgui.h"
 
 void GUI::Installer::Images::Render() {
+	static bool jpeg, png, bmp, gif, tiff, svg, webp, pdf;
+
 	if (ImGui::BeginTabItem("Images")) {
-		ImGui::Text("Placeholder");
+		ImGui::Text("Supported Image Types:");
+		ImGui::Separator();
+
+		ImGui::Checkbox("JPEG", &jpeg);
+		ImGui::Checkbox("PNG", &png);
+		ImGui::Checkbox("BMP", &bmp);
+		ImGui::Checkbox("GIF", &gif);
+		ImGui::Checkbox("TIFF", &tiff);
+		ImGui::Checkbox("SVG", &svg);
+		ImGui::Checkbox("WEBP", &webp);
+		ImGui::Checkbox("PDF", &pdf);
+
 		ImGui::EndTabItem();
 	}
 }
